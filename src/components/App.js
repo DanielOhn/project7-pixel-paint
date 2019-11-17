9189536f718e0cabc0a8433c4825ce42d6d91683
unknown <ohndaniel@gmail.com>
2019-11-16 18:28:37 -0700

    Add functions to change size and colors.  Add buttons for new sizes and buttons.

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css'
 
 function App() {
-  const [context, setContext] = useState()
-  const [canvas, setCanvas] = useState()
-
   const mouse = { x: 0, y: 0 }
+  var canv
+  var ctx
 
-  function getCanvas() {
-    const canv = document.getElementById('drawing')
-    var ctx = canv.getContext('2d')
+  var background = '#555'
 
+  useEffect(() => {
     const painting = document.getElementById('paint')
     const paint_style = getComputedStyle(painting)
+
+    canv = document.getElementById('drawing')
+    ctx = canv.getContext('2d')
 
     canv.width = parseInt(paint_style.getPropertyValue('width'))
     canv.height = parseInt(paint_style.getPropertyValue('height'))
 
-    ctx.lineWidth = 3
+    ctx.lineWidth = 6
     ctx.lineJoin = 'round'
     ctx.lineCap = 'round'
     ctx.strokeStyle = '#00CC99'
+  }, [])
 
-    setCanvas(canv)
-    setContext(ctx)
+  const changeSize = size => {
+    ctx.lineWidth = size
+  }
+
+  const changeColor = color => {
+    ctx.strokeStyle = color
+  }
+
+  const changeEraser = () => {
+    ctx.strokeStyle = background
   }
 
   const mouseMove = () => {
-    canvas &&
-      canvas.addEventListener(
+    canv &&
+      canv.addEventListener(
         'mousemove',
         e => {
-          const rect = canvas.getBoundingClientRect()
+          const rect = canv.getBoundingClientRect()
 
           mouse.x = e.clientX - rect.left
           mouse.y = e.clientY - rect.top
@@ -41,45 +51,73 @@ function App() {
   }
 
   const mouseDown = () => {
-    canvas.addEventListener(
+    canv.addEventListener(
       'mousedown',
       () => {
-        context.beginPath()
-        context.moveTo(mouse.x, mouse.y)
-        canvas.addEventListener('mousemove', onPaint, false)
+        ctx.beginPath()
+        ctx.moveTo(mouse.x, mouse.y)
+        canv.addEventListener('mousemove', onPaint, false)
       },
       false
     )
   }
 
   const mouseUp = () => {
-    canvas.addEventListener(
+    canv.addEventListener(
       'mouseup',
       () => {
-        canvas.removeEventListener('mousemove', onPaint, false)
+        canv.removeEventListener('mousemove', onPaint, false)
       },
       false
     )
   }
 
   const onPaint = () => {
-    context.lineTo(mouse.x, mouse.y)
-    context.stroke()
+    ctx.lineTo(mouse.x, mouse.y)
+    ctx.stroke()
   }
+
+  // selection
+  
 
   return (
     <div className="App">
       <h1>Paintin' Bois</h1>
-      <div id="paint">
-        <canvas
-          id="drawing"
-          onMouseMove={mouseMove}
-          onMouseDown={mouseDown}
-          onMouseUp={mouseUp}
-        ></canvas>
+      <div className="content">
+        <div id="paint">
+          <canvas
+            id="drawing"
+            onMouseMove={mouseMove}
+            onMouseDown={mouseDown}
+            onMouseUp={mouseUp}
+          ></canvas>
+        </div>
+        <div className="colors">
+          <h2>Colors</h2>
+          <div className="button-list">
+          <button
+            style={{ backgroundColor: '#00CC99' }}
+            onClick={() => changeColor('#00CC99')}
+          />
+          <button
+            style={{ backgroundColor: '#000' }}
+            onClick={() => changeColor('#000')}
+          />
+          <button
+            style={{ backgroundColor: '#FFF' }}
+            onClick={() => changeColor('#FFF')}
+          />
+          <button onClick={() => changeEraser()} />
+          </div>
+        </div>
       </div>
-      {canvas && <div>Canvas Created!</div>}
-      <button onClick={getCanvas}>create</button>
+      <div>
+        <h2>Marker Size</h2>
+        <button onClick={() => changeSize(6)}>6px</button>
+        <button onClick={() => changeSize(12)}>12px</button>
+        <button onClick={() => changeSize(18)}>18px</button>
+        <button onClick={() => changeSize(24)}>24px</button>
+      </div>
     </div>
   )
 }
